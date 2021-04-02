31719c35a81d48e148e876bc697e0db289921418
pharaouk <36641995+pharaouk@users.noreply.github.com>
2021-04-02 12:52:31 -0400

    Update traindna.py

--- torch_vae/traindna.py
+++ torch_vae/traindna.py
@@ -74,7 +74,7 @@ print(xxx.shape)
 
 train_size = int(0.8 * len(xxx))
 test_size = len(xxx) - train_size
-train_ds= TensorDataset(xxx[:[100000],xxx[:[100000])
+train_ds= TensorDataset(xxx[:100000],xxx[:100000])
 test_ds= TensorDataset(xxx[100000:124500],xxx[100000:124500])
 rec_ds= TensorDataset(xxx[124500:124574],xxx[124500:124574])
 
